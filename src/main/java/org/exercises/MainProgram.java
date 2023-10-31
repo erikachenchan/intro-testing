65f33ac018489959218a36f7c853e075016faefb
erikachan <ahelp164@gmail.com>
2023-10-31 12:38:26 +0000

    Added exercises unit tests

---
+++ src/main/java/org/exercises/MainProgram.java
@@ -0,0 +1,9 @@
+package org.exercises;
+
+public class MainProgram {
+    // update here your exercise progress
+    public static int partsCompleted() {
+        return 2;
+    }
+    
+}
---
+++ src/test/java/org/exercises/ExerciseManagerTest.java
@@ -0,0 +1,47 @@
+package org.exercises;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExerciseManagerTest {
+
+    private ExerciseManagement management;
+
+    @BeforeEach
+    public void initialize() {
+        management = new ExerciseManagement();
+    }
+
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        assertEquals(0, management.exerciseList().size());
+    }
+
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+
+    @Test
+    public void addedExerciseIsInList() {
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
+
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertTrue(management.isCompleted("New exercise"));
+    }
+
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertFalse(management.isCompleted("Some exercise"));
+    }
+}
